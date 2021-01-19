3ee684b05ba3ecea6be9724a9abbd172e24f5076
Peter <46471156+fortierpm@users.noreply.github.com>
2021-01-18 23:47:43 -0600

    Delete Animated peterFORTIER Logo directory

--- Animated peterFORTIER Logo/app.js
+++
@@ -1,7 +0,0 @@
-// JS Not needed past aquiring path lengths
-
-const logo = document.querySelectorAll("#logo path");
-
-for (let i = 0; i < logo.length; i++) {
-    console.log(`Letter ${i} is ${logo[i].getTotalLength()}`);
-}
