6571d705e0d8e0b26e41bbe222f416ba78858f4e
Peter <46471156+fortierpm@users.noreply.github.com>
2021-01-18 23:47:12 -0600

    Add files via upload

---
+++ Animated peterFORTIER Logo/app.js
@@ -0,0 +1,7 @@
+// JS Not needed past aquiring path lengths
+
+const logo = document.querySelectorAll("#logo path");
+
+for (let i = 0; i < logo.length; i++) {
+    console.log(`Letter ${i} is ${logo[i].getTotalLength()}`);
+}
